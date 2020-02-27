c6a23551e1948970b7aad376af1a41c3766b9328
AlexanderLescroart <37221611+AlexanderLescroart@users.noreply.github.com>
2020-02-27 15:32:48 +0100

    Update plate.hpp

--- plate.hpp
+++ plate.hpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <numeric>
 #include <iostream>
+#include <cmath>
 
 using namespace Eigen;
 
@@ -15,44 +16,68 @@ template<typename scalar>
 		typedef std::vector<T> TV;
 		typedef SparseMatrix<scalar> SM;
 		typedef Array<scalar, Dynamic, Dynamic> DA;
-		typedef Matrix<scalar, Dynamic, 1> RHS;
+		typedef Matrix<scalar, Dynamic, Dynamic> DM;
+		typedef Matrix<scalar, Dynamic, 1> DV;
 
 		scalar k_metal_ = 65.;
 		scalar k_plastic_ = .2;
 		scalar size_;
+		scalar deltaz_;
 		int n_;
 		scalar deltax_;
 		int N_;
-		SM F_;
-		RHS Q_;
-		scalar ldir_ = floor(.3 * n_);
-		scalar udir_ = ceil(.7 * n_);
-		inline int pos(int i, int j)
+		mutable scalar p_;
+		DA v_;
+		scalar ldir_ = (.6 * n_);
+		mutable DV T_;
+
+		inline int pos(int i, int j) const
 		{
 			return(i + n_ * j);
 		}
-	public:
-		plate(DA& v, scalar const size) :
-			size_(size),
-			n_(v.rows()),
-			deltax_(size_ / n_),
-			N_(n_* n_),
-			F_(N_, N_)
+		inline int posv(int i, int j) const
 		{
-			DA k(n_,n_);
-			Q_ = RHS::Constant(N_,0);
-			k = k_metal_ * v + k_plastic_ * (1 - v);
+			return(i + (n_ - 1) * j);
+		}
+		inline scalar meanh(scalar k1, scalar k2) const {
+			return(2 * k1 * k2 / (k1 + k2));
+		}
+		inline scalar dmeanh(scalar k1, scalar k2) const {
+			return(2 * k2 * k2 / ((k1 + k2) * (k1 + k2)));
+		}
+		inline DA conductivity(DA v) const
+		{
+			return( k_metal_ * v.pow(p_) + k_plastic_ * (1 - v.pow(p_)));
+		}
+		inline scalar conductivity_v() const
+		{
+			//todo
+		}
+		DV solve(SM lhs,DV rhs) const
+		{
+			SparseLU<SparseMatrix<double>, COLAMDOrdering<int>> solver;//checken of dit wel een goede keuze van solver is
+			solver.analyzePattern(lhs);
+			solver.factorize(lhs);
+			DV x(rhs.rows());
+			x = solver.solve(rhs);
+			return(x);
+		}
+		void set_temperature() const {
+			DA k = conductivity(v_);
+			SM K(N_, N_);
+			DV Q;
+			Q = DV::Constant(N_, 0);
 			TV coefficients;
 			V w(4);
 			for (int i = 1; i < n_ - 1; ++i)
 			{
 				for (int j = 1; j < n_ - 1; ++j)
 				{
-					Q_(pos(i, j)) = 2. * deltax_*deltax_*10e5;
-					w[0] = (k(i - 1, j) + k(i, j)) / 2;
-					w[1] = (k(i + 1, j) + k(i, j)) / 2;
-					w[2] = (k(i, j - 1) + k(i, j)) / 2;
-					w[3] = (k(i, j + 1) + k(i, j)) / 2;
+					Q(pos(i, j)) = 2. * deltax_ * deltax_ / (size_ * size_ * deltaz_);
+					w[0] = meanh(k(i - 1, j - 1), k(i - 1, j));
+					w[1] = meanh(k(i, j - 1), k(i, j));
+					w[2] = meanh(k(i - 1, j - 1), k(i, j - 1));
+					w[3] = meanh(k(i - 1, j), k(i, j));
 
 					coefficients.push_back(T(pos(i, j), pos(i - 1, j), -w[0]));
 					coefficients.push_back(T(pos(i, j), pos(i + 1, j), -w[1]));
@@ -70,12 +95,12 @@ template<typename scalar>
 			}
 			for (int i = 1; i < n_ - 1; ++i)
 			{
-				if (i > ldir_&& i < udir_)
+				if (i >= ldir_)
 				{
 					coefficients.push_back(T(pos(i, 0), pos(i, 0), 1));
-					Q_(pos(i, 0)) = 293;
+					Q(pos(i, 0)) = 293;
 					coefficients.push_back(T(pos(i, n_ - 1), pos(i, n_ - 1), 1));
-					Q_(pos(i, n_ - 1)) = 293;
+					coefficients.push_back(T(pos(i, n_ - 1), pos(i, n_ - 2), -1));
 				}
 				else
 				{
@@ -85,19 +110,55 @@ template<typename scalar>
 					coefficients.push_back(T(pos(i, n_ - 1), pos(i, n_ - 2), -1));
 				}
 			}
-			F_.setFromTriplets(coefficients.begin(), coefficients.end());
-			//std::cout << MatrixXd(F_) << std::endl;
+			K.setFromTriplets(coefficients.begin(), coefficients.end());
+			T_ = this->solve(K, Q);
 		}
-		DA temperature() const 
+	public:
+		plate(DA& v, scalar const size) :
+			size_(size),
+			deltaz_(.001),
+			n_(v.rows()),
+			deltax_(size_ / n_),
+			N_(n_* n_),
+			p_(1),
+			v_(v)
+			
 		{
-			SparseLU<SparseMatrix<double>, COLAMDOrdering<int>> solver;//checken of dit wel een goede keuze van solver is
-			solver.analyzePattern(F_);
-			solver.factorize(F_);
-			RHS x(N_);
-			DA y(N_, 1);
-			x= solver.solve(Q_);
-			y = x;//lijkt niet nodige copy maar ik kreeg het niet op een andere manier gedaan.
-			y.resize(n_, n_);
-			return(y);
+			set_temperature();
+		}
+		DA get_temperature() const {
+			return(T_);
+		}
+		SM DK() const &
+		{
+			DA k = conductivity(v_);
+			TV coefficients;
+			Matrix4d dK;//Verander hier misschien Matrix4d voor elegantie (geen gebruik scalar)
+			Vector4d temp;//same
+			for (int i = 1; i < n_ - 2; ++i)
+			{
+				for (int j = 1; j < n_ - 2; ++j)
+				{
+					dK << dmeanh(k(i, j), k(i + 1, j)) + dmeanh(k(i, j), k(i, j - 1)), -dmeanh(k(i, j), k(i + 1, j)), 0, -dmeanh(k(i, j), k(i, j - 1)),
+						-dmeanh(k(i, j), k(i + 1, j)), dmeanh(k(i, j), k(i + 1, j)) + dmeanh(k(i, j), k(i, j + 1)), -dmeanh(k(i, j), k(i, j + 1)), 0,
+						0, -dmeanh(k(i, j), k(i, j + 1)), dmeanh(k(i, j), k(i, j + 1)) + dmeanh(k(i, j), k(i - 1, j)), -dmeanh(k(i, j), k(i - 1, j)),
+						-dmeanh(k(i, j), k(i, j - 1)), 0, -dmeanh(k(i, j), k(i - 1, j)), dmeanh(k(i, j), k(i, j - 1)) + dmeanh(k(i, j), k(i - 1, j));
+					temp << T_(pos(i + 1, j)), T_(pos(i + 1, j + 1)), T_(pos(i, j + 1)), T_(pos(i, j));
+					temp = dK * temp;
+					coefficients.push_back(T(pos(i + 1, j), posv(i, j), temp(0)));
+					coefficients.push_back(T(pos(i + 1, j + 1), posv(i, j), temp(1)));
+					coefficients.push_back(T(pos(i, j + 1), posv(i, j), temp(2)));
+					coefficients.push_back(T(pos(i, j), posv(i, j), temp(3)));
+				}
+			}
+			SM DK(N_, (n_ - 1) * (n_ - 1));
+			DK.setFromTriplets(coefficients.begin(), coefficients.end());
+			return(DK);
+		}
+		void set_p(scalar p) const {
+			p_ = p;
+		}
+		scalar get_p() const {
+			return(p_);
 		}
 };
