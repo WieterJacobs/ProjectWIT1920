d8fc5e374f90d6ab52e4745eb7993983c2a1ead1
WieterJacobs <37070960+WieterJacobs@users.noreply.github.com>
2020-03-07 18:29:06 +0100

    week 4

--- fin_dif.m
+++ fin_dif.m
@@ -1,13 +1,19 @@
-function cost_p = fin_dif(v)
+function cost_p = fin_dif(v,p)
+v = reshape(v,sqrt(length(v)),sqrt(length(v)));
 cost_p = zeros(size(v,1));
 for i = 1:length(v(:))
     h = 0.0001;
     vv = v;
     
     vv(i) = v(i) + h;
-    T1 = grid_discretisatie_khoek(vv);
+    T1 = grid_discretisatie_khoek(vv(:),p);
     vv(i) = v(i) - h;
-    T2 = grid_discretisatie_khoek(vv);
-    cost_p(i) = cost_p(i) + (max(T1(:)) - max(T2(:)))/2/h;
+    T2 = grid_discretisatie_khoek(vv(:),p);
+    cost_p(i) = (cost_T(T1) - cost_T(T2))/2/h;
 end
+cost_p = cost_p(:);
 end
+
+    function c = cost_T(T)
+        c = sum(T(:).^16)^(1/16);
+    end
