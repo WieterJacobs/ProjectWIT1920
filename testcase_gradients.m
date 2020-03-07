4f7d3d380a285f8cec27c07619d187f237c6bb11
WieterJacobs <37070960+WieterJacobs@users.noreply.github.com>
2020-03-07 18:25:34 +0100

    week 4

--- testcase_gradients.m
+++ testcase_gradients.m
@@ -1,5 +1,9 @@
-v = zeros(20);
-cost1 = afkookt(v(:));
+v = zeros(20); v = v(:);
+p = 1;
+cost1 = afkookt(v,p);
+cost1 = reshape(cost1,20,20);
 figure; heatmap(cost1);
-cost2 = fin_dif(v);
+cost2 = fin_dif(v(:),p);
+cost2 = reshape(cost2,20,20);
 figure; heatmap(cost2);
+figure; heatmap((cost2-cost1)./(cost2+1));
