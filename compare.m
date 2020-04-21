0ca867f59122b6da89694cb2cb02545d67ab33f9
AlexanderLescroart <37221611+AlexanderLescroart@users.noreply.github.com>
2020-04-21 20:37:19 +0200

    Add files via upload

--- compare.m
+++ compare.m
@@ -1,16 +1,20 @@
-function [T_ok,K_ok,DK_ok,cost_p_ok] = compare(path_T,path_K,path_DK,path_cost_p)
+[T_diff,K_diff,cost_v_diff]=compare_matrices("v_matrix.txt","T_matrix.txt","K_matrix.txt","cost_v_matrix.txt",3);
+T_diff
+K_diff
+heatmap(cost_v_diff);
+
+function [T_ok,K_ok,cost_p_ok] = compare_matrices(path_v,path_T,path_K,path_cost_p,p)
     K_1=readmatrix(path_K);
     T_1=readmatrix(path_T);
-    DK_1=readmatrix(path_DK);
+    %DK_1=readmatrix(path_DK);
     cost_p_1=readmatrix(path_cost_p);
-    v=0.4*ones(4);
-    [T_2, K_2] = grid_discretisatie_khoek(v(:));
-    [cost_p_2,DK_2]=afkookt(v(:));
+    v=readmatrix(path_v);
+    [T_2, K_2] = grid_discretisatie_khoek(v(:),p);
+    cost_p_2=afkookt(v(:),p);
     %n = round(sqrt(length(K_1)));
-    %cost_p_2 = reshape(cost_p_2,n-1,n-1);
-    
+    cost_p_2=reshape(cost_p_2,20,20);
     T_ok=max(T_1-T_2);
-    K_ok=max(K_1-K_2);
-    DK_ok=max(DK_1-DK_2);
-    cost_p_ok=max(cost_p_1-cost_p_2);
+    K_ok=max(max(K_1-K_2));
+    %DK_ok=max(DK_1-DK_2);
+    cost_p_ok=abs((cost_p_1-cost_p_2));
 end
