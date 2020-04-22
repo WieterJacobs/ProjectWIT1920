cf8051358a533b4a1a33226e45570f812e1e8bb7
WieterJacobs <37070960+WieterJacobs@users.noreply.github.com>
2020-04-22 11:13:57 +0200

    test in matlab

--- opt_fmincon.m
+++ opt_fmincon.m
@@ -1,8 +1,10 @@
-n = 30;
+n = 20;
 
 fun = @objfungrad;
 
 x0 = 0.4*ones(n^2,1);
+% x0 = [zeros(0.3*n,n); ones(0.4*n,n); zeros(0.3*n,n)]; x0 = x0(:);
+% x0 = x;
 Aeq = ones(1,n^2);
 beq = .4*n^2;
 lb = zeros(n^2,1);
@@ -10,17 +12,25 @@ ub = ones(n^2,1);
 options = optimoptions('fmincon',...
     'SpecifyObjectiveGradient',true,...
     'MaxIterations',1e6,...
-    'StepTolerance',1e-16);
-%     'ConstraintTolerance',1e-6);
+    'StepTolerance',1e-16,...
+    'ConstraintTolerance',1e-16);
 x = fmincon(fun,x0,[],[],Aeq,beq,lb,ub,[],options);
 
-heatmap( reshape(x,n,n));
+figure; heatmap( reshape(x,n,n));
 
 function [f,gradf] = objfungrad(v)
 p = 3;
-f = max(grid_discretisatie_khoek(v,p));
+[T, K, Q] = grid_discretisatie_khoek(v,p);
+f = cost(T, K);
 % Gradient of the objective function:
 if nargout  > 1 || 1 == 1
     gradf = afkookt(v,p);
 end
+end
+
+function c = cost(T, K)
+    pow = 16;
+    c = sum(T(:).^pow)^(1/pow);
+%     c = Q'*T/2.532752327646418e+03;
+%     c = .5*T'*K'*T;
 end
